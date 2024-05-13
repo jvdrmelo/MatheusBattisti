7ba8010c0422590b24119aa01249242d6fc82b53
jvdrmelo <jvdrmelo@gmail.com>
2024-05-13 10:00:00 -0300

    chore: testing dynamic 'img' in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const url = "https://via.placeholder.com/200"
+
   return (
     <div className="App">
       <h1>Olá React!</h1>
       <p>Meu primeiro App</p>
+      <img src={url} alt=""/>
     </div>
   );
 }
