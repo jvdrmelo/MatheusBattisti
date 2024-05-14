c5f2057f9f8f4cff0b577b05160c2d7231832249
jvdrmelo <jvdrmelo@gmail.com>
2024-05-14 12:59:26 -0300

    chore: testing out module.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Frase from './components/Frase';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
@@ -9,7 +10,9 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
       <HelloWorld/>
+      <Frase/>
       <SayMyName nome="Matheus"/>
       <SayMyName nome="João"/>
       <SayMyName nome={nome}/>
@@ -17,6 +20,12 @@ function App() {
         nome="Rodrigo" 
         idade="28" 
         profissao="Programador" 
+        foto="https://via.placeholder.com/150" 
+      />
+      <Pessoa 
+        nome="Jefferson" 
+        idade="19" 
+        profissao="Estagiário" 
         foto="https://via.placeholder.com/150" 
       />
     </div>
