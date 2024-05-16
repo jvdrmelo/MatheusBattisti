06f361bfdcf3bb89451b6f9def253e2ae6b48a28
jvdrmelo <jvdrmelo@gmail.com>
2024-05-16 13:10:33 -0300

    Chore: Aula 10 - useState na prática - Testando useState na página de login com usuário e senha

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,19 +1,42 @@
+import { useState } from "react"
+
 function Form() {
 
     function cadastrarUsuario(e) {
         e.preventDefault()
-        console.log("Cadastrou o usuáro!")
+        console.log(`Usuário ${name} foi cadastrado com a senha: ${password}`)
     }
     
+const [name, setName] = useState('Matheus')
+const [password, setPassword] = useState('Matheus')
+
     return(
         <div>
             <h1>Meu Cadastro:</h1> 
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome" ></input>
+                    <label htmlFor="name" >Nome:</label>
+                    <input 
+                    type="text"
+                    id="name" 
+                    name="name"
+                    placeholder="Digite seu nome"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} />
                 </div>
                 <div>
-                    <input type="submit" value="Cadastrar" />
+                    <label>Senha:</label>
+                    <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Digite sua senha"
+                    onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div>
+                    <input 
+                    type="submit" 
+                    value="Cadastrar" />
                 </div> 
             </form>
         </div>  
