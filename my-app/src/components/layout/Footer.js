b31be044e54bfcb556d7c14c1098f77e96e12654
jvdrmelo <jvdrmelo@gmail.com>
2024-06-06 13:59:35 -0300

    chore: React Icons (Npm install e Aplicação)

--- my-app/src/components/layout/Footer.js
+++ my-app/src/components/layout/Footer.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import styles from './Footer.module.css'
 
 function Footer() {
-    return <footer>Rodapé</footer>
-}
+    return (
+        <footer>
+            <p>Nosso rodapé</p>
+            <ul className={styles.social_list}>
+                <li >
+                    <FaFacebook/>
+                </li>
+                <li>
+                    <FaInstagram/>
+                </li>
+                <li>
+                    <FaLinkedin/>   
+                </li>
+            </ul>
+        </footer>
+    )
+}   
 
 export default Footer
