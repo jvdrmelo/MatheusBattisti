39384435219b33b2c9e427c0811fa78b55da3660
jvdrmelo <jvdrmelo@gmail.com>
2024-06-26 10:15:42 -0300

    chore: Aula 27 - Resgatando projeto do banco de dados

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -41,7 +41,14 @@ function Projects() {
             {message && <Message type="success" msg={message} />}
             <Container customClass='start'>
                 {projects.length > 0 &&
-                    projects.map((project) => <ProjectCard name={project.name} />)}
+                    projects.map((project) => <ProjectCard 
+                        id ={project.id}
+                        name={project.name}
+                        budget={project.budget}
+                        category={project.category.name}
+                        key={project.id}
+                    />)
+                }
             </Container>
         </div>
     )
