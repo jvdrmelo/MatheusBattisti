4a7567f8388eb473ea74c60a0c855351ac8afd5a
jvdrmelo <jvdrmelo@gmail.com>
2024-06-28 13:07:11 -0300

    Aula 33 - Criando áreas de serviço

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -14,6 +14,7 @@ function Project() {
 
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -34,6 +35,7 @@ function Project() {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
         // budget validation
         if(project.budget < project.cost) {
             setMessage("O Orçamento não pode ser menor que o custo do projeto!")
@@ -62,6 +64,10 @@ function Project() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
     
     return<>{project.name ? (
@@ -94,6 +100,21 @@ function Project() {
                         </div>
                     )}
                 </div>
+                <div className={styles.service_form_container}>
+                    <h2>Adicione um serviço:</h2>
+                    <button className={styles.btn} onClick={toggleServiceForm}>
+                        {!showServiceForm ? 'Adicionar Serviço' : 'Fechar'}
+                    </button>
+                    <div>
+                        <div className={styles.project_info}>
+                            {showServiceForm && <div>formulário do serviço</div>}
+                        </div>
+                    </div>
+                </div>
+                <h2>Serviços</h2>
+                <Container customClass="start">
+                    <p>Itens de serviços</p>
+                </Container>
             </Container>
         </div>
         ) : (
